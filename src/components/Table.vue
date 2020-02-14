d574d26f708e4a4c85a73971365a3c10f47cd110
arTech <artech.vn@ya.ru>
2020-02-15 00:59:50 +0300

    render and visualize

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -4,49 +4,80 @@
       ref="selectableTable"
       selectable
       :select-mode="selectMode"
-      :items="users"
+      :busy.sync="isBusy"
+      :items="userInfo"
       :fields="fields"
       @row-selected="onRowSelected"
       responsive="sm"
     >
-      <!-- Example scoped slot for select state illustrative purposes -->
-      <template v-slot:cell(selected)="{ rowSelected }">
-        <template v-if="rowSelected">
-          <span aria-hidden="true">&check;</span>
-          <span class="sr-only">Selected</span>
-        </template>
-        <template v-else>
-          <span aria-hidden="true">&nbsp;</span>
-          <span class="sr-only">Not selected</span>
-        </template>
+      <template v-slot:table-busy>
+        <div class="text-center text-danger my-2">
+          <b-spinner class="align-middle"></b-spinner>
+          <strong>Загружаю данные...</strong>
+        </div>
       </template>
     </b-table>
+
+    <b-modal ref="graph-modal" hide-footer>
+      <template
+        v-slot:modal-title
+      >{{ selected && selected[0].first_name }} {{ selected && selected[0].last_name }} WiFi</template>
+
+      <b-container fluid >
+        <b-row class="text-center" align-v="center">
+          <b-col cols="1">
+            <div>RSSI</div>
+          </b-col>
+          <b-col class="mt-3" ref="graph-container" >
+            <bar-graph
+              title="RSSI"
+              animDuration="1s"
+              :points="rssi"
+              :width="graphW"
+              :height="graphH"
+            />
+          </b-col>
+        </b-row>
+        <b-row class="text-center" align-v="center">
+          <b-col cols="1">
+            <div>TS</div>
+          </b-col>
+          <b-col class="mt-3">
+            <bar-graph title="TS" animDuration="1s" :points="ts" :width="graphW" :height="graphH" />
+          </b-col>
+        </b-row>
+      </b-container>
+      <b-button class="mt-3" block @click="hideModal">Close Me</b-button>
+    </b-modal>
 
     <div class="api-info">{{ userInfo }}</div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
+import BarGraph from "vue-svg-charts/src/components/bar";
+//import ky from "ky";
 
 //const API_TEST = 'https://api.coindesk.com/v1/bpi/currentprice.json';
-const API_TEST = 'https://rssi.wmrk.tk/';
+const API_TEST = "https://rssi.wmrk.tk/";
 
-const WORK_API = API_TEST;
+const WORK_API = `https://cors-anywhere.herokuapp.com/${API_TEST}`;
 
 export default {
   name: "Table",
-  components: {},
+  components: { BarGraph },
   data: function() {
     return {
       fields: [
         {
-          key: "id",
+          key: "first_name",
+          label: "Имя",
           sortable: true
         },
         {
-          key: "fio",
-          label: "ФИО",
+          key: "last_name",
+          label: "Фамилия",
           sortable: true
         },
         {
@@ -60,42 +91,62 @@ export default {
           sortable: true
         }
       ],
-      users: [
-        {
-          id: 0,
-          fio: "FIO",
-          mac: "23-df-34-45-56",
-          phone: "+7-123-456-78-90"
-        },
-        {
-          id: 1,
-          fio: "FIO-123",
-          mac: "23-df-34-45-56",
-          phone: "+7-123-456-78-90"
-        },
-        {
-          id: 2,
-          fio: "FIO-3453",
-          mac: "23-df-34-45-56",
-          phone: "+7-123-456-78-90"
-        }
-      ],
-      selectMode: "single",
-      selected: null,
 
-      userInfo: null
+      selectMode: "single",
+      selected: [],
+
+      rssi: [],
+      ts: [],
+
+      userInfo: [],
+      isBusy: false,
+
+      graphW: 256,
+      graphH: 128
     };
   },
   methods: {
+    showModal() {
+      this.$refs["graph-modal"].show();
+      
+    },
+    hideModal() {
+      this.$refs["graph-modal"].hide();
+    },
     onRowSelected(items) {
+      this.rssi = [];
+      this.ts = [];
       this.selected = items;
+
+      // получаем данные сигнала выбранного элемента
+      const signals = items[0].signals;
+
+      // найдём минимальные значения уровней для нормализации графиков
+      const rssi = signals.map(signal => signal.rssi);
+      const ts = signals.map(signal => signal.ts);
+      // rssi имеет отрицательные значения
+      // нас интересует самое маленькой по абсолютному значению
+      // для отрицательных чисел это МАКСИМАЛЬНОЕ
+      const rssiMin = Math.max.apply(null, rssi) * 0.9; // считаем за минимум значние на 10% меньше минимального
+      const tsMin = Math.min.apply(null, ts);
+      console.log(`RSSI MIN = ${rssiMin}`);
+      console.log(`TS MIN = ${tsMin}`);
+
+      signals.forEach(signal => {
+        this.rssi.push(Math.abs(signal.rssi - rssiMin));
+        this.ts.push(signal.ts - tsMin);
+      });
+
+      this.showModal();
     }
   },
 
   mounted() {
-    axios
-      .get(WORK_API)
-      .then(response => (this.userInfo = response));
+    this.isBusy = true;
+    axios.get(WORK_API).then(response => {
+      this.isBusy = false;
+      this.userInfo = Object.values(response.data);
+    });
   }
 };
 </script>
