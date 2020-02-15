4fcf457cc46ff324627d78df74f6a1457d216fb6
arTech <artech.vn@ya.ru>
2020-02-16 01:23:48 +0300

    start rebuild architecture

---
+++ src/components/Chart.vue
@@ -0,0 +1,54 @@
+<template>
+    <apexchart type="line" height="350" :options="chartOptions" :series="series"></apexchart>
+</template>
+
+<script>
+import VueApexCharts from "vue-apexcharts";
+
+export default {
+    name: "Chart",
+    components: { apexchart: VueApexCharts },
+    data: function() {
+        return {
+            // параметры графика вывода данных
+            series: [
+                {
+                    name: "RSSI",
+                    data: []
+                }
+            ],
+            chartOptions: {
+                chart: {
+                    height: 350,
+                    type: "line",
+                    zoom: {
+                        enabled: true
+                    }
+                },
+                xaxis: {
+                    type: "numeric",
+                    labels: {
+                        formatter: function(value) {
+                            // переводим время в универсальное для подписи строки координат
+                            const date = new Date(value);
+
+                            const hours = date.getUTCHours();
+                            const minutes = date.getUTCMinutes();
+                            const seconds = date.getUTCSeconds();
+                            const milliseconds = date.getUTCMilliseconds();
+
+                            return `${hours}:${minutes}:${seconds}'${milliseconds}`; // The formatter function overrides format property
+                        }
+                    }
+                },
+                tooltip: {
+                    x: {
+                        formatter: function(val) {
+                            return new Date(val);
+                        }
+                    }
+                }
+            },
+        }},
+};
+</script>
